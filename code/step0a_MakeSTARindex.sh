2cd9c213837dd670e4f80d84d44bf1caff9f2052
mgandal <mgandal@gmail.com>
2016-08-16 12:19:13 -0700

    t

--- code/step0a_MakeSTARindex.sh
+++ code/step0a_MakeSTARindex.sh
@@ -2,8 +2,8 @@
 
 STAR_call=/home/mgandal/bin/STAR-2.5.2a/bin/Linux_x86_64/STAR
 
-qrsh -l h_rt=24:00:00,h_data=80G -pe shared 8
-$STAR_call
+#qrsh -l h_rt=24:00:00,h_data=80G -pe shared 8
+$STAR_call \
   --runThreadN 8 \
   --runMode genomeGenerate \
   --genomeDir /hp_shares/mgandal/datasets/refGenome/mmul10/GencodeM10/Sequence/STAR_index \
