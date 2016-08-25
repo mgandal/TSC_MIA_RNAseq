4634195fa213baf184ecfd5d9090a3c15403668a
Michael Gandal <mgandal@gmail.com>
2016-08-24 19:41:31 -0700

    na

--- code/step3a_HTSeq.sh
+++ code/step3a_HTSeq.sh
@@ -7,5 +7,5 @@ cd $bamdir
 
 for bam in */*Aligned.out.bam; do
     echo $bam
-    qsub -V -cwd -e $codedir/log -o $codedir/log -l h_rt=04:00:00,h_data=8G,highp $codedir/qsub_htseq.sh $bamdir/$bam
+    qsub -V -cwd -e $codedir/log -o $codedir/log -l h_rt=04:00:00,h_data=8G,highp $codedir/step3b_qsub_htseq.sh $bamdir/$bam
 done
